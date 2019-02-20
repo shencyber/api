92a014cdf5028a0a19eac537e2cfe4d8c607c3ae
cyber <1725854865@qq.com>
2019-02-20 17:08:25 +0800

    准备解决删除分类问题

--- application/index/controller/Category.php
+++ application/index/controller/Category.php
@@ -54,6 +54,45 @@ class Category extends Controller
     }
 
     /**
+     * [deleteCateById 删除分类 ,如果该分类下有商品则无法删除]
+     * @return [type] [description]
+     */
+    public function deleteCateById()
+    {
+        $param = Request::instance()->param();
+        $cateId = $param['cateId'] ;
+
+        $obj = Array("status"=>0 , "desc"=>"");
+        //1、判断该分类是否存在
+        $exists = Db::table( $this->table )->where( "id" , $cateId )->find() ;
+        if( $exists == NULL ) 
+        {
+
+            return json_encode(Array("status"=>1,"desc"=>"分类不存在") , JSON_UNESCAPED_UNICODE) ;
+            die;
+        }
+
+        //2、查询该分类下是否有商品
+        $exists = DB::table('goods')->where('cateid' , $cateId )->find();
+        if(  $exists == NULL )
+        {
+             $res =  Db::table( $this->table )->delete( $cateId ); 
+
+            if( $res > 0 ) 
+                return json_encode(Array("status"=>0,"desc"=>"删除成功") , JSON_UNESCAPED_UNICODE) ;
+            else 
+                return json_encode(Array("status"=>-1,"desc"=>"删除失败") , JSON_UNESCAPED_UNICODE) ;   
+
+        } 
+        else
+        {
+            return json_encode(Array("status"=>2,"desc"=>"该分类下有商品,无法删除分类") , JSON_UNESCAPED_UNICODE) ;
+            die;
+        }
+        
+    }
+
+    /**
      * 根据供货商id获取所有分类
      */
     public function getCateList(  )
